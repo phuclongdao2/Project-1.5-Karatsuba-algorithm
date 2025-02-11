8ba350ab757a795734d671e61bb1dc4df798935f
Đào Phúc Long <146737950+phuclongdao2@users.noreply.github.com>
2025-02-11 23:05:47 +0700

    Update header.hpp

--- header.hpp
+++ header.hpp
@@ -8,7 +8,7 @@
 #include <vector>
 using Polynomial = std::vector<long long>;
 
-Polynomial Add(const long long* a, int size_a, const long long* b, int size_b);
+void Add(const long long* a, int size_a, const long long* b, int size_b, long long* Result);
 Polynomial BruteForceMultiply(const long long* a, int size_a, const long long* b, int size_b);
 void ParallelKaratsuba(const long long* a, const long long* b, int size, Polynomial& Result, int depth);
 void PrepareKaratsuba(const Polynomial& A, const Polynomial& B, Polynomial& Result, int depth);
