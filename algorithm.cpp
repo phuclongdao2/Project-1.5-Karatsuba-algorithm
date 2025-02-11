a43448f5caec251dc350e1b0b694e89a6436cd39
Đào Phúc Long <146737950+phuclongdao2@users.noreply.github.com>
2025-02-11 23:06:42 +0700

    Dồn sumA, sumB chung vector

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,19 +1,9 @@
 #include "header.hpp"
 
-//Cộng hai đa thức A, B
-Polynomial Add(const long long* a, int size_a, const long long* b, int size_b) {
-    if (size_a > size_b) {
-        Polynomial Result(size_a);
-        std::copy_n(a, size_a, Result.begin());
-        for (int i = 0; i < size_b; ++i) Result[i] += b[i];
-        return Result;
-    }
-    else {
-        Polynomial Result(size_b);
-        std::copy_n(b, size_b, Result.begin());
-        for (int i = 0; i < size_a; ++i) Result[i] += a[i];
-        return Result;
-    }
+//Cộng hai đa thức A, B (deg A <= deg B)
+void Add(const long long* a, int size_a, const long long* b, int size_b, long long* Result) {
+    std::copy_n(b, size_b, Result);
+    std::transform(a, a + size_a, Result, Result, std::plus<>());
 }
 
 //Thuật toán trực tiếp nhân hai đa thức A, B
@@ -35,14 +25,15 @@ void ParallelKaratsuba(const long long* a, const long long* b, int size, Polynom
         Polynomial P1(midsize);
         Polynomial P2(highsize);
         Polynomial P3(highsize);
-        Polynomial sumA = Add(a, mid, a + mid, high);
-        Polynomial sumB = Add(b, mid, b + mid, high);
+        Polynomial sumAsumB(high << 1);
+        Add(a, mid, a + mid, high, sumAsumB.data());
+        Add(b, mid, b + mid, high, &sumAsumB[high]);
 
         //Có sử dụng tính toán song song
         if (depth) {
-            std::thread thread1(ParallelKaratsuba, a, b, mid, std::ref(P1), depth - 1);
-            std::thread thread2(ParallelKaratsuba, a + mid, b + mid, high, std::ref(P2), depth - 1);
-            ParallelKaratsuba(&sumA[0], &sumB[0], high, P3, depth - 1);
+            std::thread thread1([&] { ParallelKaratsuba(a, b, mid, P1, depth - 1); });
+            std::thread thread2([&] { ParallelKaratsuba(a + mid, b + mid, high, P2, depth - 1); });
+            ParallelKaratsuba(sumAsumB.data(), &sumAsumB[high], high, P3, depth - 1);
             thread1.join(); thread2.join();
         }
 
@@ -50,15 +41,15 @@ void ParallelKaratsuba(const long long* a, const long long* b, int size, Polynom
         else {
             ParallelKaratsuba(a, b, mid, P1, 0);
             ParallelKaratsuba(a + mid, b + mid, high, P2, 0);
-            ParallelKaratsuba(&sumA[0], &sumB[0], high, P3, 0);
+            ParallelKaratsuba(sumAsumB.data(), &sumAsumB[high], high, P3, 0);
         }
 
-        std::copy_n(&P1[0], midsize, Result.begin());
-        std::copy_n(&P2[0], highsize, Result.begin() + mid * 2);
+        std::copy_n(P1.begin(), midsize, Result.begin());
+        std::copy_n(P2.begin(), highsize, Result.begin() + mid * 2);
         Result[midsize] = 0;
         int i = 0;
         for (; i < midsize; ++i) Result[i + mid] += P3[i] - P1[i] - P2[i];
-        if (size % 2) {
+        if (size & 1) {
             Result[i + mid] += P3[i] - P2[i];
             ++i;
             Result[i + mid] += P3[i] - P2[i];
@@ -71,14 +62,14 @@ void PrepareKaratsuba(const Polynomial& A, const Polynomial& B, Polynomial& Resu
     int size = std::max(A.size(), B.size());
     Result.resize((size << 1) - 1);
     if (A.size() < B.size()) {
-        Polynomial A_resized(A);
-        A_resized.resize(size, 0);
-        ParallelKaratsuba(&A_resized[0], &B[0], size, Result, depth);
+        Polynomial A_resized(size, 0);
+        std::copy(A.begin(), A.end(), A_resized.begin());
+        ParallelKaratsuba(A_resized.data(), B.data(), size, Result, depth);
     }
     else if (A.size() > B.size()) {
-        Polynomial B_resized(B);
-        B_resized.resize(size, 0);
-        ParallelKaratsuba(&A[0], &B_resized[0], size, Result, depth);
+        Polynomial B_resized(size, 0);
+        std::copy(B.begin(), B.end(), B_resized.begin());
+        ParallelKaratsuba(A.data(), B_resized.data(), size, Result, depth);
     }
-    else ParallelKaratsuba(&A[0], &B[0], size, Result, depth);
+    else ParallelKaratsuba(A.data(), B.data(), size, Result, depth);
 }
