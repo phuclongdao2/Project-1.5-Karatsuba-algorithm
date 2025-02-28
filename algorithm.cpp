84ade5651ba357387726747d279f407eaa58bff2
Đào Phúc Long <146737950+phuclongdao2@users.noreply.github.com>
2025-02-28 17:13:59 +0700

    Code cleanup algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -21,7 +21,7 @@ void ParallelKaratsuba(const long long* a, const long long* b, int size, long lo
     else {
         int mid = size >> 1, high = size - mid;
         int mid2 = mid << 1, midsize = mid2 - 1, highsize = (high << 1) - 1;
-        long long* resmid = result + mid, * resmid2 = result + mid2;
+        long long *resmid = result + mid, *resmid2 = result + mid2;
         //P1: Result[0...mid * 2 - 2], P2 : Result[mid * 2...size * 2 - 2]
         //sumA: sumAsumB[0...high-1], sumB: sumAsumB[high...high*2-1]
         Polynomial P3(highsize), sumAsumB(highsize + 1);
@@ -48,7 +48,7 @@ void ParallelKaratsuba(const long long* a, const long long* b, int size, long lo
         for (; i < mid; ++i) resmid[i] += (P3[i] -= result[i]) - resmid2[i];
         for (; i < midsize; ++i) resmid[i] = P3[i - mid] + P3[i] - result[i] - resmid2[i];
         if (size & 1) {
-            resmid[i] += P3[i] - resmid2[i];
+            resmid[midsize] += P3[midsize] - resmid2[midsize];
             resmid[mid2] += P3[mid2] - resmid2[mid2];
         }
     }
@@ -59,14 +59,15 @@ void PrepareKaratsuba(const Polynomial& A, const Polynomial& B, Polynomial& Resu
     int size = std::max(A.size(), B.size());
     Result.resize((size << 1) - 1);
     if (A.size() < B.size()) {
-        Polynomial A_resized(size, 0);
-        std::copy(A.begin(), A.end(), A_resized.begin());
+        Polynomial A_resized = A;
+        A_resized.resize(size, 0);
         ParallelKaratsuba(A_resized.data(), B.data(), size, Result.data(), depth);
     }
     else if (A.size() > B.size()) {
-        Polynomial B_resized(size, 0);
-        std::copy(B.begin(), B.end(), B_resized.begin());
+        Polynomial B_resized = B;
+        B_resized.resize(size, 0);
         ParallelKaratsuba(A.data(), B_resized.data(), size, Result.data(), depth);
     }
     else ParallelKaratsuba(A.data(), B.data(), size, Result.data(), depth);
+    Result.resize(A.size() + B.size() - 1);
 }
