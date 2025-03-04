c94ddaad734031a83ad5d75f43a4f8b7d4d1a968
Đào Phúc Long <146737950+phuclongdao2@users.noreply.github.com>
2025-03-04 18:16:30 +0700

    Dồn P3, sumAsumB chung vector

--- algorithm.cpp
+++ algorithm.cpp
@@ -11,7 +11,7 @@ void BruteForceMultiply(const long long* a, int size_a, const long long* b, int 
     for (int i = 0; i < size_a; ++i) {
         long long temp = a[i];
         for (int j = 0; j < size_b; ++j) result[j] += temp * b[j];
-        result++;
+        ++result;
     }
 }
 
@@ -19,37 +19,37 @@ void BruteForceMultiply(const long long* a, int size_a, const long long* b, int 
 void ParallelKaratsuba(const long long* a, const long long* b, int size, long long* result, int depth) {
     if (size <= 100) BruteForceMultiply(a, size, b, size, result);
     else {
-        int mid = size >> 1, high = size - mid;
-        int mid2 = mid << 1, midsize = mid2 - 1, highsize = (high << 1) - 1;
-        long long *resmid = result + mid, *resmid2 = result + mid2;
-        //P1: Result[0...mid * 2 - 2], P2 : Result[mid * 2...size * 2 - 2]
-        //sumA: sumAsumB[0...high-1], sumB: sumAsumB[high...high*2-1]
-        Polynomial P3(highsize), sumAsumB(highsize + 1);
-        Add(a, mid, a + mid, high, sumAsumB.data());
-        Add(b, mid, b + mid, high, &sumAsumB[high]);
+        //P1: Result[0 ... mid * 2 - 2], P2 : Result[mid * 2 ... size * 2 - 2]
+        //sumA: P3[high * 2 - 1 ... high * 3 - 2], sumB: P3[high * 3 - 1 ... high * 4 - 2]
+        int mid = size >> 1, high = size - mid, mid2 = mid << 1, midsize = mid2 - 1;
+        long long* resmid = result + mid, * P2 = result + mid2;
+        Polynomial P3((high << 2) - 1);
+        long long* sumA = P3.data() + (high << 1) - 1, * sumB = sumA + high;
+        Add(a, mid, a + mid, high, sumA);
+        Add(b, mid, b + mid, high, sumB);
 
         //Có sử dụng tính toán song song
         if (depth) {
             std::thread thread1([&] { ParallelKaratsuba(a, b, mid, result, depth - 1); });
-            std::thread thread2([&] { ParallelKaratsuba(a + mid, b + mid, high, resmid2, depth - 1); });
-            ParallelKaratsuba(sumAsumB.data(), &sumAsumB[high], high, P3.data(), depth - 1);
+            std::thread thread2([&] { ParallelKaratsuba(a + mid, b + mid, high, P2, depth - 1); });
+            ParallelKaratsuba(sumA, sumB, high, P3.data(), depth - 1);
             thread1.join(); thread2.join();
         }
 
         //Không sử dụng tính toán song song
         else {
             ParallelKaratsuba(a, b, mid, result, 0);
-            ParallelKaratsuba(a + mid, b + mid, high, resmid2, 0);
-            ParallelKaratsuba(sumAsumB.data(), &sumAsumB[high], high, P3.data(), 0);
+            ParallelKaratsuba(a + mid, b + mid, high, P2, 0);
+            ParallelKaratsuba(sumA, sumB, high, P3.data(), 0);
         }
 
         result[midsize] = 0;
         int i = 0;
-        for (; i < mid; ++i) resmid[i] += (P3[i] -= result[i]) - resmid2[i];
-        for (; i < midsize; ++i) resmid[i] = P3[i - mid] + P3[i] - result[i] - resmid2[i];
+        for (; i < mid; ++i) resmid[i] += (P3[i] -= result[i]) - P2[i];
+        for (; i < midsize; ++i) resmid[i] = P3[i - mid] + P3[i] - result[i] - P2[i];
         if (size & 1) {
-            resmid[midsize] += P3[midsize] - resmid2[midsize];
-            resmid[mid2] += P3[mid2] - resmid2[mid2];
+            resmid[midsize] += P3[midsize] - P2[midsize];
+            resmid[mid2] += P3[mid2] - P2[mid2];
         }
     }
 }
